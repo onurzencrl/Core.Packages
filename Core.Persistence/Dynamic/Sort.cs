59fcf0b50019606b5b5c38a41c3c72180c1df710
onurzencirli <onurzencirli>
2024-01-26 13:50:22 +0300

    Add project files.

---
+++ Core.Persistence/Dynamic/Sort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Dynamic
+{
+    public class Sort
+    {
+        public string Dir { get; set; }
+        public string Field { get; set; }
+
+        public Sort()
+        {
+            Field = string.Empty;
+            Dir = string.Empty;
+        }
+        public Sort(string field, string dir)
+        {
+            Field = field;
+            Dir = dir;
+        }
+    }
+}
